ffa2394cf3d7493bbfe3970034a5c619e10d9fbc
Gina Vegetti <ggvegetti@gmail.com>
2024-07-22 18:42:07 -0300

    imagenes

--- Practico-04-Modulo-1-main/js/admin.js
+++
@@ -1,9 +0,0 @@
-
-let usuario = prompt("Ingrese su usuario")
-let contraseña = prompt ("Ingrese su contreseña")
-
-if (usuario === "admin" && contraseña === "admin123" ) {
-    alert ("Bienvenido, admin.");
-} else {
-    alert ("Acceso denegado.");
-}
